bc0b23958ae449d387b1739b77745b3689b9c18b
Devansh Shah <devanshnshah11@gmail.com>
2024-01-09 18:51:43 +0530

    Added_course_info

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  );
+};
+
+const Content = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
+  console.log(course);
+  console.log(part1, part2, part3);
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1.name} exercises={part1.exercises} />
+      <Content part={part2.name} exercises={part2.exercises} />
+      <Content part={part3.name} exercises={part3.exercises} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+    </div>
+  );
+};
+
+export default App;
