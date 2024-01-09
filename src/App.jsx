ad6c1148097cad928d8bb23afebbf2d61d9bdabb
Devansh Shah <devanshnshah11@gmail.com>
2024-01-09 18:57:08 +0530

    Exercise 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -22,27 +22,28 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   console.log(course);
-  console.log(part1, part2, part3);
   return (
     <div>
       <Header course={course} />
-      <Content part={part1.name} exercises={part1.exercises} />
-      <Content part={part2.name} exercises={part2.exercises} />
-      <Content part={part3.name} exercises={part3.exercises} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content part={parts[0].name} exercises={parts[0].exercises} />
+      <Content part={parts[1].name} exercises={parts[1].exercises} />
+      <Content part={parts[2].name} exercises={parts[2].exercises} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
     </div>
   );
 };
